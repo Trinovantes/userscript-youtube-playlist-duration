8f7efd3f0c0196067384db52cacaeed8e7a6f2eb
Stephen Li <stephenli.ca@gmail.com>
2022-08-27 23:34:11 -0400

    Ignore videos without valid titles/duration

--- src/services/ytb/renderDurationOnPlaylistPage.ts
+++ src/services/ytb/renderDurationOnPlaylistPage.ts
@@ -8,27 +8,35 @@ export async function renderDurationOnPlaylistPage() {
     }
 
     const videoRows = await findDelayedElement('#contents.ytd-playlist-video-list-renderer > ytd-playlist-video-renderer')
-    console.info(DEFINE.NAME, 'renderDurationOnPlaylistPage', `videos:${videoRows.length}`)
+    console.info(DEFINE.NAME, 'renderDurationOnPlaylistPage()', `videos:${videoRows.length}`)
 
     const durationNodeId = `${DEFINE.NAME}-duration`
-    const durationNode = document.getElementById(durationNodeId) ?? $(`<span style="display:block;" id="${durationNodeId}"></span>`)
+    const durationNode = document.getElementById(durationNodeId) ?? $(`<strong style="display:block;" id="${durationNodeId}"></strong>`)
     $(durationNode).text('Duration: (?)')
 
-    const headerContainer = await findDelayedElement('ytd-playlist-sidebar-renderer h1#title')
+    const headerContainer = await findDelayedElement('ytd-playlist-sidebar-renderer #stats')
     headerContainer.append(durationNode)
 
     const durationTracker = new DurationTracker()
     for (const videoRow of videoRows) {
-        const durationWrapper = await findDelayedElement('span.ytd-thumbnail-overlay-time-status-renderer', videoRow)
-        const duration = durationWrapper.text().trim()
-
         const titleWrapper = await findDelayedElement('a#video-title', videoRow)
-        const title = titleWrapper.text().trim()
+        if (titleWrapper.length !== 1) {
+            console.warn('Invalid titleWrapper', titleWrapper)
+            continue
+        }
 
+        const durationWrapper = await findDelayedElement('span.ytd-thumbnail-overlay-time-status-renderer', videoRow)
+        if (durationWrapper.length !== 1) {
+            console.warn('Invalid durationWrapper', durationWrapper)
+            continue
+        }
+
+        const title = titleWrapper.text().trim()
+        const duration = durationWrapper.text().trim()
         const durationInSec = durationTracker.addDuration(duration)
         console.debug(DEFINE.NAME, `"${title}": Parsed ${duration} as ${durationInSec} seconds`)
     }
 
-    console.info(DEFINE.NAME, 'renderDurationOnPlaylistPage', `duration:${durationTracker.duration}`)
+    console.info(DEFINE.NAME, 'renderDurationOnPlaylistPage()', `duration:${durationTracker.duration}`)
     $(durationNode).text(`Duration: ${durationTracker.duration}`)
 }
--- src/services/ytb/renderDurationOnPlaylistVideo.ts
+++ src/services/ytb/renderDurationOnPlaylistVideo.ts
@@ -8,7 +8,7 @@ export async function renderDurationOnPlaylistVideo() {
     }
 
     const playlistItems = await findDelayedElement('#content #container #items.playlist-items.ytd-playlist-panel-renderer > ytd-playlist-panel-video-renderer')
-    console.info(DEFINE.NAME, 'renderDurationOnPlaylistVideo', `videos:${playlistItems.length}`)
+    console.info(DEFINE.NAME, 'renderDurationOnPlaylistVideo()', `videos:${playlistItems.length}`)
 
     const durationNodeId = `${DEFINE.NAME}-duration`
     const durationNode = document.getElementById(durationNodeId) ?? $(`<span style="display:block;" id="${durationNodeId}"></span>`)
@@ -19,16 +19,24 @@ export async function renderDurationOnPlaylistVideo() {
 
     const durationTracker = new DurationTracker()
     for (const playlistItem of playlistItems) {
-        const durationWrapper = await findDelayedElement('span.ytd-thumbnail-overlay-time-status-renderer', playlistItem)
-        const duration = durationWrapper.text().trim()
-
         const titleWrapper = await findDelayedElement('span#video-title', playlistItem)
-        const title = titleWrapper.text().trim()
+        if (titleWrapper.length !== 1) {
+            console.warn('Invalid titleWrapper', titleWrapper)
+            continue
+        }
 
+        const durationWrapper = await findDelayedElement('span.ytd-thumbnail-overlay-time-status-renderer', playlistItem)
+        if (durationWrapper.length !== 1) {
+            console.warn('Invalid durationWrapper', durationWrapper)
+            continue
+        }
+
+        const title = titleWrapper.text().trim()
+        const duration = durationWrapper.text().trim()
         const durationInSec = durationTracker.addDuration(duration)
         console.debug(DEFINE.NAME, `"${title}": Parsed ${duration} as ${durationInSec} seconds`)
     }
 
-    console.info(DEFINE.NAME, 'renderDurationOnPlaylistVideo', `duration:${durationTracker.duration}`)
+    console.info(DEFINE.NAME, 'renderDurationOnPlaylistVideo()', `duration:${durationTracker.duration}`)
     $(durationNode).text(`Duration: ${durationTracker.duration}`)
 }
