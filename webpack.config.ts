f43c6ecd0393bd44bf37b58dcaa54ad2eb1c9df7
Stephen Li <stephenli.ca@gmail.com>
2023-03-17 02:10:11 -0400

    Upgrade to webpack-userscript 3

--- webpack.config.ts
+++ webpack.config.ts
@@ -39,9 +39,11 @@ const config: webpack.Configuration = {
         ? 'development'
         : 'production',
 
-    entry: path.resolve(srcDir, 'main.ts'),
+    entry: {
+        [packageJson.name]: path.resolve(srcDir, 'main.ts'),
+    },
     output: {
-        filename: `${packageJson.name}.user.js`,
+        path: distDir,
     },
 
     resolve: {
@@ -96,7 +98,7 @@ const config: webpack.Configuration = {
                 name: packageJson.productName,
                 version: isDev
                     ? '0.0.0'
-                    : '[version]',
+                    : packageJson.version,
                 match: [
                     '*://*.youtube.com/*',
                 ],
@@ -108,10 +110,15 @@ const config: webpack.Configuration = {
                     'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js',
                 ],
             },
-            proxyScript: {
-                enable: isDev,
-                baseUrl,
-            },
+            ...(isDev
+                ? {
+                    proxyScript: {
+                        baseURL: `${baseUrl}/`,
+                        filename: '[basename].proxy.user.js',
+                    },
+                }
+                : {}
+            ),
         }),
     ],
 }
